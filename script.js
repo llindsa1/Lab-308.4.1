3501b5f72c403b5d7b9759e9c6b5b15307702c86
llindsa1 <lindsayuchiha@icloud.com>
2024-09-16 03:22:30 -0400

    third commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Feeling Loopy
+// Part 1: Refactoring Old Code
 
 // input - ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26
 
@@ -75,3 +75,7 @@ for (let i in string) {
       console.log(row) // Print row
     }
   }
+
+  let candidates = ['Bruce', 'Bob', 'Blaine', 'Bill']
+  
+
